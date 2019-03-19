79264f3ae166d41e2341698915a6cbb6d1929c9e
aiko3310 <aiko3310@hotmail.com>
2019-03-19 13:56:23 +0800

    Updates

---
+++ precache-manifest.d815a7a91c14c391e38c6fcb3a4e7683.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "253bddf42581a737230c3ffb1b40dd81",
+    "url": "/The-F2E/static/media/week2.253bddf4.jpg"
+  },
+  {
+    "revision": "ab375c23bb838e02ea4193e86d452fd0",
+    "url": "/The-F2E/static/media/week1.ab375c23.jpg"
+  },
+  {
+    "revision": "f865ccfd054aa1493ad2d9ab11ee9f8e",
+    "url": "/The-F2E/static/media/nodata.f865ccfd.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/The-F2E/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "c70a78765b8fad737f8f",
+    "url": "/The-F2E/static/js/runtime~main.c70a7876.js"
+  },
+  {
+    "revision": "63c6ad29d54caeb3d9f6",
+    "url": "/The-F2E/static/js/main.63c6ad29.chunk.js"
+  },
+  {
+    "revision": "368040f186c4118b6e45",
+    "url": "/The-F2E/static/js/2.368040f1.chunk.js"
+  },
+  {
+    "revision": "63c6ad29d54caeb3d9f6",
+    "url": "/The-F2E/static/css/main.b67ed19e.chunk.css"
+  },
+  {
+    "revision": "368040f186c4118b6e45",
+    "url": "/The-F2E/static/css/2.9c67cb9c.chunk.css"
+  },
+  {
+    "revision": "66286bb7c0d378de489f85ed27e03a88",
+    "url": "/The-F2E/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.e6e5f610f75fb073173b756122145985.js"
+  "/The-F2E/precache-manifest.d815a7a91c14c391e38c6fcb3a4e7683.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/The-F2E/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
